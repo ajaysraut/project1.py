b5d1e70fed3a70f5ff5416048a910651cb93dadb
ajaysraut <ajaysraut97@gmail.com>
2023-01-10 19:19:54 +0530

    first commit

---
+++ project_reminder.py
@@ -0,0 +1,11 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title = " ** Please drink water now !",
+            message = " Please drink water for your health, Daily you want to drink 3 to 4 lit water for your body",
+            timeout = 30
+        )
+        time.sleep(60*60)    
